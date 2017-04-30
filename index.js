a5f8759e84480163cf30d40ed4ddc6586ecbdabd
Ethan Cohen <ethanvcohen@gmail.com>
2017-04-30 17:09:40 -0400

    Add all tooling.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var tmp = require('tmp');
-var fs = require('fs');
+const tmp = require('tmp');
+const fs = require('fs');
 
 function isReactComponent(source = '') {
   return source.indexOf('(_react.Component)') > -1;
@@ -10,40 +10,39 @@ function WebpackAccessibilityPlugin(options) {
   this.renderMarkup = options.renderMarkup;
 }
 
-WebpackAccessibilityPlugin.prototype.apply = function(compiler) {
-  compiler.plugin('emit', function(compilation, callback) {
+WebpackAccessibilityPlugin.prototype.apply = (compiler) => {
+  compiler.plugin('emit', (compilation, callback) => {
     // Explore each chunk (build output):
-    compilation.chunks.forEach(function(chunk) {
+    compilation.chunks.forEach((chunk) => {
       // Start with application specific modules
       chunk.modules
         .filter(module => module.resource && module.resource.indexOf('node_modules') === -1 && module.resource.match(/\.(js|jsx)$/))
         .map(module => module._source._value)
         .filter(isReactComponent)
-        .forEach(function(source) {
+        .forEach((source) => {
           // Write to temporary file
-          tmp.file({ postfix: '.js', dir: './tmp' }, function _tempFileCreated(err, path, fd, cleanupCallback) {
-            if (err) throw err;
-            var self = this;
+          tmp.file({ postfix: '.js', dir: './tmp' }, (tmpErr, path, fd, cleanupCallback) => {
+            if (tmpErr) throw tmpErr;
+            const self = this;
 
             fs.writeFile(path, source, (err) => {
               if (err) throw err;
 
-              var component = require('./' + path).default;
-              var element = self.createElement(component);
-              var markup = self.renderMarkup(element);
+              const component = require(path).default;
+              const element = self.createElement(component);
+              const markup = self.renderMarkup(element);
 
               // Run a11y report on markup!
               console.log(markup);
 
               cleanupCallback();
             });
-          }.bind(this));
+          });
         }, this);
     }, this);
 
     callback();
-  }.bind(this));
+  });
 };
 
 module.exports = WebpackAccessibilityPlugin;
-
