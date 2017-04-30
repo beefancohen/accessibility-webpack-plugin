498e61ee24fc7bc144a1a64560f680194077b160
Ethan Cohen <ethanvcohen@gmail.com>
2017-04-30 17:31:10 -0400

    use class syntax for better this support.

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 const tmp = require('tmp');
 const fs = require('fs');
 
-function isReactComponent(source = '') {
-  return source.indexOf('(_react.Component)') > -1;
-}
+const isReactComponent = source => source.indexOf('(_react.Component)') > -1;
 
-function WebpackAccessibilityPlugin(options) {
-  this.createElement = options.createElement;
-  this.renderMarkup = options.renderMarkup;
-}
+class AccessibilityWebpackPlugin {
+  constructor(options) {
+    this.createElement = options.createElement;
+    this.renderMarkup = options.renderMarkup;
+  }
 
-WebpackAccessibilityPlugin.prototype.apply = (compiler) => {
-  compiler.plugin('emit', (compilation, callback) => {
-    // Explore each chunk (build output):
-    compilation.chunks.forEach((chunk) => {
-      // Start with application specific modules
-      chunk.modules
-        .filter(module => module.resource && module.resource.indexOf('node_modules') === -1 && module.resource.match(/\.(js|jsx)$/))
-        .map(module => module._source._value) // eslint-disable-line no-underscore-dangle
-        .filter(isReactComponent)
-        .forEach((source) => {
-          // Write to temporary file
-          tmp.file({ postfix: '.js', dir: `${__dirname}/tmp` }, (tmpErr, path, fd, cleanupCallback) => {
-            if (tmpErr) throw tmpErr;
+  apply(compiler) {
+    compiler.plugin('emit', (compilation, callback) => {
+      // Explore each chunk (build output):
+      compilation.chunks.forEach((chunk) => {
+        // Start with application specific modules
+        chunk.modules
+          .filter(module => module.resource && module.resource.indexOf('node_modules') === -1 && module.resource.match(/\.(js|jsx)$/))
+          .map(module => module._source._value) // eslint-disable-line no-underscore-dangle
+          .filter(isReactComponent)
+          .forEach((source) => {
+            // Write to temporary file
+            tmp.file({ postfix: '.js', dir: `${__dirname}/tmp` }, (tmpErr, path, fd, cleanupCallback) => {
+              if (tmpErr) throw tmpErr;
 
-            fs.writeFile(path, source, (err) => {
-              if (err) throw err;
+              fs.writeFile(path, source, (err) => {
+                if (err) throw err;
 
-              const component = require(path).default; // eslint-disable-line
-              const element = this.createElement(component);
-              const markup = this.renderMarkup(element);
+                const component = require(path).default; // eslint-disable-line
+                const element = this.createElement(component);
+                const markup = this.renderMarkup(element);
 
-              // Run a11y report on markup!
-              console.log(markup); // eslint-disable-line
+                // Run a11y report on markup!
+                console.log(markup); // eslint-disable-line
 
-              cleanupCallback();
+                cleanupCallback();
+              });
             });
           });
-        }, this);
-    }, this);
+      });
 
-    callback();
-  });
-};
+      callback();
+    });
+  }
+}
 
-module.exports = WebpackAccessibilityPlugin;
+module.exports = AccessibilityWebpackPlugin;
