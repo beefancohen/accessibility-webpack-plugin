f24b445fda7ccd8d18b530e5533aa3c2f2a07838
Ethan Cohen <ethanvcohen@gmail.com>
2017-05-01 00:43:36 -0400

    WIP - add support for non js file imports.

--- index.js
+++ index.js
@@ -3,11 +3,21 @@ require('babel-polyfill');
 const tmp = require('tmp');
 const fs = require('fs');
 const chalk = require('chalk');
-const rimraf = require('rimraf');
+// const rimraf = require('rimraf');
 
 const TMP_DIR = `${__dirname}/tmp`;
+const FILE_MOCK_DEPENDENCY_PATH = `${__dirname}/fileMock`;
+const STYLE_MOCK_DEPENDENCY_PATH = `${__dirname}/styleMock`;
+const JS_FILE_REGEX = /\.(js|jsx)$/;
+const FILE_MOCK_REGEX = /\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$/;
+const STYLE_MOCK_REGEX = /\.(css|less|sass)$/;
 
 const isReactComponent = source => source.indexOf('(_react.Component)') > -1;
+
+const isExternalDependency = module =>
+  module && module.resource && module.resource.indexOf('node_modules') > -1;
+
+const getSourceCode = module => module._source._value;
 
 const writeModule = source =>
   new Promise((resolve, reject) => {
@@ -34,37 +44,47 @@ const writeModule = source =>
   });
 
 const handleDependencies = async (module) => {
-  const { dependencies: moduleDependencies = [] } = module;
+  try {
+    const { dependencies: moduleDependencies = [] } = module;
 
-  const dependencies = moduleDependencies
-    .filter(
-      dependency =>
-        dependency.module &&
-        dependency.module.resource &&
-        dependency.module.resource.indexOf('node_modules') === -1,
-    )
-    .map(dependency => dependency.module);
+    const dependencies = moduleDependencies
+      .map(dependency => dependency.module)
+      .filter(dependency => isExternalDependency(dependency) === false);
 
-  if (dependencies.length === 0) {
-    return module;
+    if (dependencies.length === 0) {
+      return module;
+    }
+
+    return await dependencies.reduce(async (modifiedModule = {}, dependency) => {
+      // Recurse through all dependencies if it is a JS file.
+      await handleDependencies(dependency);
+
+      const pathToReplaceInSource = dependency.rawRequest;
+      const source = modifiedModule.modifiedSource || getSourceCode(modifiedModule);
+      const dependencyPaths = modifiedModule.dependencyPaths || [];
+
+      if (STYLE_MOCK_REGEX.test(dependency.userRequest)) {
+        return {
+          ...module,
+          modifiedSource: source.replace(pathToReplaceInSource, STYLE_MOCK_DEPENDENCY_PATH),
+        };
+      } else if (FILE_MOCK_REGEX.test(dependency.userRequest)) {
+        return {
+          ...module,
+          modifiedSource: source.replace(pathToReplaceInSource, FILE_MOCK_DEPENDENCY_PATH),
+        };
+      }
+
+      const newSourcePath = await writeModule(getSourceCode(dependency));
+      return {
+        ...module,
+        modifiedSource: source.replace(pathToReplaceInSource, newSourcePath),
+        dependencyPaths: dependencyPaths.concat([newSourcePath]),
+      };
+    }, module);
+  } catch (e) {
+    throw e;
   }
-
-  let dependencyPaths = [];
-  const moduleSource = await dependencies.reduce(async (source = '', dependency) => {
-    // Recurse through all dependencies.
-    await handleDependencies(dependency);
-
-    const pathToReplace = dependency.rawRequest;
-    const newSourcePath = await writeModule(dependency._source._value);
-    dependencyPaths = dependencyPaths.concat([newSourcePath]);
-    return source.replace(pathToReplace, newSourcePath);
-  }, module._source._value);
-
-  return {
-    ...module,
-    modifiedSource: moduleSource,
-    dependencyPaths,
-  };
 };
 
 class AccessibilityWebpackPlugin {
@@ -78,28 +98,34 @@ class AccessibilityWebpackPlugin {
       compilation.chunks.forEach((chunk) => {
         chunk.modules
           .filter(
-            module =>
-              module.resource &&
-              module.resource.indexOf('node_modules') === -1 &&
-              module.resource.match(/\.(js|jsx)$/),
+            module => isExternalDependency(module) === false && JS_FILE_REGEX.test(module.resource),
           )
-          .filter(module => isReactComponent(module._source._value))
+          .filter(module => isReactComponent(getSourceCode(module)))
           .forEach(async (module) => {
-            // const fileName = module.resource;
-            const { modifiedSource, dependencyPaths = [] } = await handleDependencies(module);
-            const source = modifiedSource || module._source._value;
+            try {
+              const { modifiedSource } = await handleDependencies(module);
+              const source = modifiedSource || getSourceCode(module);
 
-            writeModule(source).then((path) => {
-              const component = require(path).default; // eslint-disable-line
-              const element = this.createElement(component);
-              const markup = this.renderMarkup(element);
+              writeModule(source)
+                .then((path) => {
+                  const component = require(path).default; // eslint-disable-line
+                  const element = this.createElement(component);
+                  const markup = this.renderMarkup(element);
 
-              // Run a11y report on markup!
-              console.log(chalk.green(`<${component.name}>: `), markup, '\n'); // eslint-disable-line
+                  // Run a11y report on markup!
+                  console.log(chalk.green(`<${component.name}>: `), markup, '\n'); // eslint-disable-line
 
-              // Clean up tmp files.
-              dependencyPaths.concat([path]).forEach(file => rimraf(file, () => {}));
-            });
+                  // Clean up tmp files.
+                  // TODO: create glob from list of paths to make this one rimraf call.
+                  // dependencyPaths.concat([path]).forEach(file => rimraf(file, () => {}));
+                })
+                .catch((e) => {
+                  console.log({ e });
+                  throw e;
+                });
+            } catch (e) {
+              throw e;
+            }
           });
       });
 
---
+++ styleMock.js
@@ -0,0 +1,3 @@
+// This is a mock for all files that match the following regex:
+// /\.(css|less|sass)$/
+module.exports = {};
